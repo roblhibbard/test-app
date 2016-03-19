59542dbe6e7730203d6fc7cc5346889401e03331
Robert <robert@ezbc.net>
2016-03-19 12:04:06 -0600

    create users table and model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
      @article = Article.new(article_params)
     
      if @article.save
-       flash[:notice] = "Article was successfully created"
+       flash[:success] = "Article was successfully created"
        redirect_to article_path(@article)
       else
 
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
       
     
       if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
+        flash[:danger] = "Article was successfully updated"
         redirect_to article_path(@article)
       else
         render 'edit'
